f469f29212c2a34af69282c57ca208d280e8079a
Nikolas Padilla <muddxyii@gmail.com>
2026-01-26 11:39:30 -0700

    Replace SVG favicon with ICO favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 
