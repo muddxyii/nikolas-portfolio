ac0c37b9563cee975fe7833cbb324b5673d3a22b
Nikolas Padilla <muddxyii@gmail.com>
2026-01-26 10:13:57 -0700

    Set up projects content system with mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -4,8 +4,8 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: { adapter: adapter() },
-	preprocess: [mdsvex()],
-	extensions: ['.svelte', '.svx']
+	preprocess: [mdsvex({ extensions: ['.md'] })],
+	extensions: ['.svelte', '.md']
 };
 
 export default config;
