49cae9fc995f9abc4b3a20eb396d45e437ddee48
Nikolas Padilla <muddxyii@gmail.com>
2026-05-25 15:45:14 -0700

    Add portfolio OG image

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -27,6 +27,9 @@ const ogImage = $derived(image.startsWith('http') ? image : `${siteConfig.url}${
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 	<meta property="og:image" content={ogImage} />
+	<meta property="og:image:width" content="1200" />
+	<meta property="og:image:height" content="630" />
+	<meta property="og:image:alt" content="Nikolas Padilla, Software Engineer" />
 	<meta property="og:url" content={canonical} />
 	<meta property="og:type" content={type} />
 	<meta property="og:site_name" content={siteConfig.name} />
@@ -34,4 +37,5 @@ const ogImage = $derived(image.startsWith('http') ? image : `${siteConfig.url}${
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
 	<meta name="twitter:image" content={ogImage} />
+	<meta name="twitter:image:alt" content="Nikolas Padilla, Software Engineer" />
 </svelte:head>
